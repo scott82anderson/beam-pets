48b0ac6d5675bed7e608936855056f50570def54
Scott Anderson <mail@scottanderson.com.au>
2021-11-20 21:45:37 +1100

    Added pet count to People Container.

--- web/src/components/PeopleContainer/PeopleList/index.js
+++ web/src/components/PeopleContainer/PeopleList/index.js
@@ -13,6 +13,7 @@ const PeopleList = ({ people }) => {
             <a href={`/person/${person.id}`}>{person.name}</a>
           </h4>
           <p>{person.description}</p>
+          <p>Pets: {person.pets.length}</p>
         </li>
       ))}
     </ul>
